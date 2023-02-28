a4ff769459c3dbe41af18488d60b336f4c85ea38
Alanjim Halabia <alanjim.halabia@hotmail.com>
2023-02-28 01:40:36 -0500

    feat: game grid setup

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('#container');
+for (i=0;i<16*16;i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    container.append(content);
+}
