bcf22efe62902718e9c843d11af4cca8370fa80b
Alanjim Halabia <alanjim.halabia@hotmail.com>
2023-02-28 03:01:16 -0500

    feat: colour selector and clear board

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Etch - A - Sketch
-Working replica of the Etch - A - Sketch toy which was made popular in the 90s and early 00s. The mouse replicates the use of the dial on the original product.
+Working replica of the Etch - A - Sketch toy which was made popular in the 90s and early 00s. The mouse replicates the use of a pen on the original product.
--- javascript.js
+++ javascript.js
@@ -1,12 +1,47 @@
 const container = document.querySelector('#container');
+
+// create 16x16 grid
 for (i=0;i<16*16;i++) {
     const content = document.createElement('div');
     content.classList.add('content');
     container.append(content);
-    const button = document.getElementsByClassName("content");
-    button[i].addEventListener('mouseenter', function (e) {
-    e.target.style.background = 'blue';
-  });
 }
 
+let etch = (picker) => {
+    // fills in boxes with user-selected colour
+for (i=0;i<16*16;i++) {
+    const button = document.getElementsByClassName("content");
+    button[i].addEventListener('mouseenter', function (e) {
+    e.target.style.background = picker;
+    });
+}
+}
+
+let erase = () => {
+    // clears all boxes
+for (i=0;i<16*16;i++) {
+    const button = document.getElementsByClassName("content");
+    button[i].style.backgroundColor = "white";
+    };
+}
+
+
+// change fill colour based on user selection
+const redButton = document.querySelector('#red');
+redButton.addEventListener('click', () => {
+    etch("red")
+  });
+const blueButton = document.querySelector('#blue');
+blueButton.addEventListener('click', () => {
+    etch("blue")
+});
+const greenButton = document.querySelector('#green');
+greenButton.addEventListener('click', () => {
+    etch("green")
+});
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', function (e) {
+    erase()
+});
 
