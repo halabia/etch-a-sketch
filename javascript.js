3852aa77027d2e8ad3c32286f1205c1472c06921
Alanjim Halabia <alanjim.halabia@hotmail.com>
2023-02-28 01:56:06 -0500

    feat: initial functionality

--- javascript.js
+++ javascript.js
@@ -3,4 +3,10 @@ for (i=0;i<16*16;i++) {
     const content = document.createElement('div');
     content.classList.add('content');
     container.append(content);
+    const button = document.getElementsByClassName("content");
+    button[i].addEventListener('mouseenter', function (e) {
+    e.target.style.background = 'blue';
+  });
 }
+
+
